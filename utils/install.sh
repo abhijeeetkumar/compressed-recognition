44bfcecb1be51530149624e1cc8f4595e3907ca3
Kumar, Abhijeet <abhijeet.kumar@psu.edu>
2021-02-16 18:59:15 -0500

    Using ffmpeg internals to build video partial decoder

---
+++ utils/install.sh
@@ -0,0 +1,3 @@
+rm -rf build #TODO: remove hardcoding of py3.8.3 
+/home/mdl/amk7371/Python-3.8.3_lib/bin/python3 setup.py build_ext --inplace
+/home/mdl/amk7371/Python-3.8.3_lib/bin/python3 setup.py install --user
---
+++ utils/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+import numpy as np
+
+utils_module = Extension('cva',
+		sources = ['main.c'],
+		include_dirs=[np.get_include(), './ffmpeg_lib/include/'],
+		extra_compile_args=['-DNDEBUG', '-O3', '-std=c99'],
+		extra_link_args=['-L./ffmpeg_lib/lib/']
+)
+
+setup ( name = 'cva',
+	version = '0.1',
+	description = 'Utils for compressed video analytics (cva).',
+	ext_modules = [ utils_module ]
+)
