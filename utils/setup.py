5087984c29443acb545d17c8c985d5ea1d97e048
Kumar, Abhijeet <abhijeet.kumar@psu.edu>
2021-03-30 03:24:20 -0400

    adding parser for qp at each CU

--- utils/setup.py
+++ utils/setup.py
@@ -3,9 +3,9 @@ import numpy as np
 
 utils_module = Extension('cva',
 		sources = ['main.c'],
-		include_dirs=[np.get_include(), '/home/mdl/amk7371/ffmpeg_lib/include/'],
+		include_dirs=[np.get_include(), '/home/mdl/amk7371/video_analytics/FFmpeg_x64/include/'],
 		extra_compile_args=['-DNDEBUG', '-O3', '-std=c99'],
-		extra_link_args=['-L/home/mdl/amk7371/ffmpeg_lib/lib/']
+		extra_link_args=['-L/home/mdl/amk7371/video_analytics/FFmpeg_x64/include/', '-lavutil', '-lavcodec', '-lavformat', '-lswresample', '-lswscale']
 )
 
 setup ( name = 'cva',
